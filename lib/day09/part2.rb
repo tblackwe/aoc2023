c1494cd3abc153983bc635c5680ef3fded514198
Thomas Blackwell <thomas.blackwell@coforma.io>
2023-12-09 20:18:24 -0500

    part 2 was remarkably easy

--- lib/day09/part2.rb
+++ lib/day09/part2.rb
@@ -2,6 +2,15 @@
 
 # Day09 of AoC 2023
 class Day09
-  input = File.readlines('./lib/day09/test_input')
-  
+  input = File.readlines('./lib/day09/input').map(&:chomp)
+
+  values = []
+
+  input.each do |line|
+    extrapolated = [line.split.map(&:to_i).reverse]
+    extrapolated << extrapolated.last.each_cons(2).map { |a, b| b - a } until extrapolated.last&.all?(&:zero?)
+    to_add = extrapolated.pop.last + (to_add || 0) until extrapolated.size == 1
+    values << extrapolated.last.last + to_add
+  end
+  pp values.sum
 end
