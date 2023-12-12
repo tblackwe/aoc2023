9012b0ef28b68801d9545d29373b340830590e64
Thomas Blackwell <thomas.blackwell@coforma.io>
2023-12-12 11:23:57 -0500

    day 5 fast part1

--- lib/day05/part1.rb
+++ lib/day05/part1.rb
@@ -2,31 +2,40 @@
 
 # Day05 of AoC 2023
 class Day05
-  input = File.readlines('./lib/day05/test_input')
-  seeds_to_plant = input.first[6..].strip.split
-  location_map = {}
+  # Arrange
+  input = File.readlines('./lib/day05/input')
+  location_map = { 'seeds' => { indicies: input.first[6..].strip.split.map(&:to_i) } }
   current_key = ''
+
   input[2..].each do |line|
     next if line.strip == ''
 
     if /(\w*) map:/.match(line)
       current_key = /(.*) map:/.match(line)[1]
-      location_map[current_key] = { raw_data: [], arr: [] }
+      location_map[current_key] = { raw_data: [], indicies: [] }
       next
     end
+    location_map[current_key][:raw_data] << line.strip.split.map(&:to_i)
+  end
 
-    location_map[current_key][:raw_data] << line.strip.split
-    puts location_map[current_key].prev
-    item = line.strip.split
-    root_num = item[0].to_i
-    start = item[1].to_i
-    count = item[2].to_i
-    place_holder = (0..count - 1).map { |i| i + root_num }
-    location_map[current_key][:arr][start..(start + count - 1)] = place_holder
+  # Act
+  previous_key = 'seeds'
+  %w[seed-to-soil soil-to-fertilizer fertilizer-to-water water-to-light light-to-temperature temperature-to-humidity
+     humidity-to-location].each do |key|
+    temp = location_map[key][:raw_data].reverse
+
+    location_map[previous_key][:indicies].each do |index|
+      ph = index
+      temp.each do |range|
+        next unless index.between?(range[1], range[1] + range[2])
+
+        ph = range[0] - range[1] + index
+      end
+      previous_key = key
+      location_map[key][:indicies] << ph
+    end
   end
-  seeds_to_plant.each do |seed|
-    seed_i = seed.to_i
-    puts "Seed #{seed}, soil #{location_map['seed-to-soil'][:arr][seed_i]}, fertilizer #{location_map['seed-to-soil'][:arr][seed_i]}, water #{location_map['seed-to-soil'][:arr][seed_i]}, light #{location_map['seed-to-soil'][:arr][seed_i]}, temperature #{location_map['seed-to-soil'][:arr][seed_i]}, humidity #{location_map['seed-to-soil'][:arr][seed_i]}, location X"
-  end
-  # this doesnt work, quitting on 5 for now
+
+  # Answer
+  pp location_map['humidity-to-location'][:indicies].min
 end
